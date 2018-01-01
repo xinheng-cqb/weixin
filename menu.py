b8dd392ac77a1e90db922acbf22444508403c8b5
caiqibin <qibin.c@winbaoxian.com>
2018-01-01 15:46:21 +0800

    menu function test

---
+++ menu.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# @Time    : 2018/1/1 15:40
+# @Author  : xinheng-cqb
+# @Software: PyCharm Community Edition
+# @introduce:
+
+import urllib
+from basic import Basic
+
+class Menu(object):
+    def __init__(self):
+        pass
+    def create(self, postData, accessToken):
+        postUrl = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=%s" % accessToken
+        if isinstance(postData, unicode):
+            postData = postData.encode('utf-8')
+        urlResp = urllib.urlopen(url=postUrl, data=postData)
+        print urlResp.read()
+
+    def query(self, accessToken):
+        postUrl = "https://api.weixin.qq.com/cgi-bin/menu/get?access_token=%s" % accessToken
+        urlResp = urllib.urlopen(url=postUrl)
+        print urlResp.read()
+
+    def delete(self, accessToken):
+        postUrl = "https://api.weixin.qq.com/cgi-bin/menu/delete?access_token=%s" % accessToken
+        urlResp = urllib.urlopen(url=postUrl)
+        print urlResp.read()
+
+    #获取自定义菜单配置接口
+    def get_current_selfmenu_info(self, accessToken):
+        postUrl = "https://api.weixin.qq.com/cgi-bin/get_current_selfmenu_info?access_token=%s" % accessToken
+        urlResp = urllib.urlopen(url=postUrl)
+        print urlResp.read()
+
+if __name__ == '__main__':
+    myMenu = Menu()
+    postJson = """
+    {
+        "button":
+        [
+            {
+                "name": "公众平台",
+                "sub_button":
+                [
+                    {
+                        "type": "view",
+                        "name": "更新公告",
+                        "url": "http://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1418702138&token=&lang=zh_CN"
+                    },
+                    {
+                        "type": "view",
+                        "name": "接口权限说明",
+                        "url": "http://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1418702138&token=&lang=zh_CN"
+                    },
+                    {
+                        "type": "view",
+                        "name": "返回码说明",
+                        "url": "http://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1433747234&token=&lang=zh_CN"
+                    }
+                ]
+            },
+            {
+                "type": "media_id",
+                "name": "趣图",
+                "media_id": "d0CabfzyOQXxzZ6X5-TuaEMBaPuV-dIINaxuTaz5DlWqo6IHgjXSHgn2le_4SDbq"
+            },
+            {
+                "type": "click",
+                "name": "段子",
+                "key":  "mpGuide"
+            }
+          ]
+    }
+    """
+    Basic().real_get_access_token()
+    accessToken = Basic().get_access_token()
+    myMenu.create(postJson, accessToken)
---
+++ service/media.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# @Time    : 2017/12/29 19:49
+# @Author  : xinheng-cqb
+# @Software: PyCharm Community Edition
+# @introduce: 
